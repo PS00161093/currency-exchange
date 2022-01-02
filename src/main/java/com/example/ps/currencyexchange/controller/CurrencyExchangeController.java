8b928eb8b85f54c79e6f56dc62ed8bb327b59f9a
Prashant Sharma <prashant.sharma1thoughtworks.com>
2022-01-02 18:10:10 +0530

    Added currency-exchange controller & bean

---
+++ src/main/java/com/example/ps/currencyexchange/controller/CurrencyExchangeController.java
@@ -0,0 +1,21 @@
+package com.example.ps.currencyexchange.controller;
+
+import com.example.ps.currencyexchange.bean.CurrencyExchange;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(
+            @PathVariable String from,
+            @PathVariable String to) {
+
+        return new CurrencyExchange(1000L,from, to, BigDecimal.valueOf(50) );
+    }
+
+}
