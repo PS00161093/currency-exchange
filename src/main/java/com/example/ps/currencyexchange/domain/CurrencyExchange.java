ed618dc6bc57916e9b3634dc6f5decfd3e0fe475
Prashant Sharma <prashant.sharma1thoughtworks.com>
2022-01-02 18:30:37 +0530

    Added JPA & connection to DB for CurrencyExchange

--- src/main/java/com/example/ps/currencyexchange/domain/CurrencyExchange.java
+++ src/main/java/com/example/ps/currencyexchange/domain/CurrencyExchange.java
@@ -1,13 +1,26 @@
-package com.example.ps.currencyexchange.bean;
+package com.example.ps.currencyexchange.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class CurrencyExchange {
 
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
+
     private BigDecimal conversionMultiple;
+
     private String environment;
 
     public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
